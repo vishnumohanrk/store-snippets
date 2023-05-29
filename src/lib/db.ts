c167fed68d8fa710d76d3d3a5c37eb6ee61d3439
vishnumohanrk <mohanvish1999@gmail.com>
2023-05-29 20:17:51 +0530

    clerk, prisma init

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import 'server-only';
+
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db;
+}
